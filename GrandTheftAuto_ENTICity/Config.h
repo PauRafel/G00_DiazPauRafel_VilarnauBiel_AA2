1f0e8ede348aea73a5a64a4ad044c9773c48c0c5
Pau Rafel Díaz Hernández <paurafel.diaz@enti.cat>
2025-05-03 11:23:44 +0200

    Config.h base

--- GrandTheftAuto_ENTICity/Config.h
+++ GrandTheftAuto_ENTICity/Config.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <string>
+
+struct ConfigData {
+    int mapWidth;
+    int mapHeight;
+
+    int pedestriansLosSantos;
+    int tollLosSantos;
+    int maxMoneyLosSantos;
+
+    int pedestriansSanFierro;
+    int tollSanFierro;
+    int maxMoneySanFierro;
+};
+
+// Función para cargar la configuración
+ConfigData loadConfig(const std::string& filename);
